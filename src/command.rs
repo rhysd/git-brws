b682350e7b10cd14334d98cfc8b934ef10b680b1
rhysd <lin90162@yahoo.co.jp>
2016-12-28 07:28:50 +0900

    open URL with 'open' crate

--- src/command.rs
+++ src/command.rs
@@ -1,3 +1,5 @@
+extern crate open;
+
 use std::path::PathBuf;
 use page::parse_page;
 use service;
@@ -17,12 +19,26 @@ pub fn url(opts: Options) -> Result<String, ErrorMsg> {
     service::parse_url(&opts.repo, &opts.branch, &page)
 }
 
+fn open(url: String) -> Option<ErrorMsg> {
+    match open::that(&url) {
+        Ok(status) => {
+            if status.success() {
+                None
+            } else {
+                if let Some(code) = status.code() {
+                    Some(format!("Error on opening URL {}: Command exited with non-zero status {}", url, code))
+                } else {
+                    Some(format!("Error on opening URL {}: Command terminated by signal", url))
+                }
+            }
+        },
+        Err(e) => Some(format!("Error on opening URL {}: {}", url, e))
+    }
+}
+
 pub fn browse(opts: Options) -> Option<ErrorMsg> {
     match url(opts) {
-        Ok(url) => {
-            println!("TODO: Open browser with URL {}", url);
-            None
-        },
+        Ok(url) => open(url),
         Err(msg) => Some(msg),
     }
 }
