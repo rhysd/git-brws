07fb62d0096db6043791d261b3116791463410af
rhysd <lin90162@yahoo.co.jp>
2016-12-28 16:40:31 +0900

    refactor: reduce duplicates on building page URL

--- src/service.rs
+++ src/service.rs
@@ -6,17 +6,17 @@ use page::Page;
 
 use util;
 
-fn build_github_url(user: &str, repo: &str, branch: &Option<String>, page: &Page) -> String {
+fn build_github_like_url(host: &str, user: &str, repo: &str, branch: &Option<String>, page: &Page) -> String {
     match page {
         &Page::Open => if let &Some(ref b) = branch {
-            format!("https://github.com/{}/{}/tree/{}", user, repo, b)
+            format!("https://{}/{}/{}/tree/{}", host, user, repo, b)
         } else {
-            format!("https://github.com/{}/{}", user, repo)
+            format!("https://{}/{}/{}", host, user, repo)
         },
-        &Page::Diff {ref lhs, ref rhs} => format!("https://github.com/{}/{}/compare/{}...{}", user, repo, lhs, rhs),
-        &Page::Commit {ref hash} => format!("https://github.com/{}/{}/commit/{}", user, repo, hash),
-        &Page::FileOrDir {ref relative_path, ref hash, line: None} => format!("https://github.com/{}/{}/blob/{}/{}", user, repo, hash, relative_path),
-        &Page::FileOrDir {ref relative_path, ref hash, line: Some(line)} => format!("https://github.com/{}/{}/blob/{}/{}#L{}", user, repo, hash, relative_path, line),
+        &Page::Diff {ref lhs, ref rhs} => format!("https://{}/{}/{}/compare/{}...{}", host, user, repo, lhs, rhs),
+        &Page::Commit {ref hash} => format!("https://{}/{}/{}/commit/{}", host, user, repo, hash),
+        &Page::FileOrDir {ref relative_path, ref hash, line: None} => format!("https://{}/{}/{}/blob/{}/{}", host, user, repo, hash, relative_path),
+        &Page::FileOrDir {ref relative_path, ref hash, line: Some(line)} => format!("https://{}/{}/{}/blob/{}/{}#L{}", host, user, repo, hash, relative_path, line),
     }
 }
 
@@ -41,34 +41,6 @@ fn build_bitbucket_url(user: &str, repo: &str, branch: &Option<String>, page: &P
     }
 }
 
-fn build_github_enterprise_url(host: &str, user: &str, repo: &str, branch: &Option<String>, page: &Page) -> String {
-    match page {
-        &Page::Open => if let &Some(ref b) = branch {
-            format!("https://{}/{}/{}/tree/{}", host, user, repo, b)
-        } else {
-            format!("https://{}/{}/{}", host, user, repo)
-        },
-        &Page::Diff {ref lhs, ref rhs} => format!("https://{}/{}/{}/compare/{}...{}", host, user, repo, lhs, rhs),
-        &Page::Commit {ref hash} => format!("https://{}/{}/{}/commit/{}", host, user, repo, hash),
-        &Page::FileOrDir {ref relative_path, ref hash, line: None} => format!("https://{}/{}/{}/blob/{}/{}", host, user, repo, hash, relative_path),
-        &Page::FileOrDir {ref relative_path, ref hash, line: Some(line)} => format!("https://{}/{}/{}/blob/{}/{}#L{}", host, user, repo, hash, relative_path, line),
-    }
-}
-
-fn build_gitlab_url(user: &str, repo: &str, branch: &Option<String>, page: &Page) -> String {
-    match page {
-        &Page::Open => if let &Some(ref b) = branch {
-            format!("https://gitlab.com/{}/{}/tree/{}", user, repo, b)
-        } else {
-            format!("https://gitlab.com/{}/{}", user, repo)
-        },
-        &Page::Diff {ref lhs, ref rhs} => format!("https://gitlab.com/{}/{}/compare/{}...{}", user, repo, lhs, rhs),
-        &Page::Commit {ref hash} => format!("https://gitlab.com/{}/{}/commit/{}", user, repo, hash),
-        &Page::FileOrDir {ref relative_path, ref hash, line: None} => format!("https://gitlab.com/{}/{}/blob/{}/{}", user, repo, hash, relative_path),
-        &Page::FileOrDir {ref relative_path, ref hash, line: Some(line)} => format!("https://gitlab.com/{}/{}/blob/{}/{}#L{}", user, repo, hash, relative_path, line),
-    }
-}
-
 // Note: Parse '/user/repo.git' or '/user/repo' or 'user/repo' into 'user' and 'repo'
 fn user_and_repo_from_path<'a>(path: &'a str) -> util::Result<(&'a str, &'a str)> {
     let mut split = path.split('/').skip_while(|s| s.is_empty());
@@ -82,19 +54,18 @@ fn user_and_repo_from_path<'a>(path: &'a str) -> util::Result<(&'a str, &'a str)
 }
 
 // Known URL formats
-//  https://hosting_service.com/user/repo.git
-//  git@hosting_service.com:user/repo.git (-> ssh://git@hosting_service.com:22/user/repo.git)
+//  1. https://hosting_service.com/user/repo.git
+//  2. git@hosting_service.com:user/repo.git (-> ssh://git@hosting_service.com:22/user/repo.git)
 pub fn parse_and_build_page_url(repo: &String, page: &Page, branch: &Option<String>) -> util::Result<String> {
     let url = Url::parse(repo).map_err(|e| format!("{}", e))?;
     let path = url.path();
     let (user, repo_name) = user_and_repo_from_path(path)?;
     let host = url.host_str().ok_or(format!("Failed to parse host from {}", repo))?;
     match host {
-        "github.com" => Ok(build_github_url(user, repo_name, branch, page)),
+        "github.com" | "gitlab.com" => Ok(build_github_like_url(host, user, repo_name, branch, page)),
         "bitbucket.org" => build_bitbucket_url(user, repo_name, branch, page),
-        "gitlab.com" => Ok(build_gitlab_url(user, repo_name, branch, page)),
-        host => if host.starts_with("github.") {
-            Ok(build_github_enterprise_url(host, user, repo_name, branch, page))
+        _ => if host.starts_with("github.") {
+            Ok(build_github_like_url(host, user, repo_name, branch, page))
         } else {
             Err(format!("Unknown hosting service for URL {}", repo))
         },
