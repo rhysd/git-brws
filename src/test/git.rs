a8c1668f941e31704e2d418848fbf758837030a7
rhysd <lin90162@yahoo.co.jp>
2019-04-28 02:46:13 +0900

    add more tests for git.rs

--- src/test/git.rs
+++ src/test/git.rs
@@ -1,3 +1,4 @@
+use crate::error::Error;
 use crate::git::{git_dir, Git};
 use std::env;
 
@@ -40,6 +41,53 @@ fn git_get_commit_hash() {
 }
 
 #[test]
+fn git_get_invalid_hash() {
+    let g = Git::new(".git".as_ref(), "git");
+    match g.hash("HEAD~114514") {
+        Err(Error::GitObjectNotFound { kind, .. }) => assert_eq!(kind, "commit"),
+        r => assert!(false, "Unexpected result: {:?}", r),
+    }
+}
+
+#[test]
+fn git_get_tag_hash() {
+    let g = Git::new(".git".as_ref(), "git");
+    let h = g.tag_hash("0.10.1").unwrap();
+    assert_eq!(h, "601e6c33bb760d8e7d5684a75eec1f0257b8ff22");
+}
+
+#[test]
+fn git_get_invalid_tag_hash() {
+    let g = Git::new(".git".as_ref(), "git");
+    match g.tag_hash("this-tag-is-not-existing") {
+        Err(Error::GitObjectNotFound { kind, .. }) => assert_eq!(kind, "tag name"),
+        r => assert!(false, "Unexpected result: {:?}", r),
+    }
+}
+
+#[test]
+fn git_get_remote_url() {
+    let g = Git::new(".git".as_ref(), "git");
+    let u = g.remote_url("origin").unwrap();
+    assert!(
+        u.starts_with("git@") || u.starts_with("https://"),
+        "invalid url: {:?}",
+        u
+    );
+    assert!(u.contains("git-brws"), "invalid url: {:?}", u);
+    assert!(u.ends_with(".git"), "invalid url: {:?}", u);
+}
+
+#[test]
+fn git_get_invalid_remote_url() {
+    let g = Git::new(".git".as_ref(), "git");
+    match g.remote_url("this-remote-is-not-existing") {
+        Err(Error::GitObjectNotFound { kind, .. }) => assert_eq!(kind, "remote"),
+        r => assert!(false, "Unexpected result: {:?}", r),
+    }
+}
+
+#[test]
 fn git_root_dir() {
     let g = Git::new(".git".as_ref(), "git");
     let p = g.root_dir().unwrap();
@@ -49,7 +97,7 @@ fn git_root_dir() {
 #[test]
 fn git_check_remote_contains() {
     let g = Git::new(".git".as_ref(), "git");
-    g.remote_contains("HEAD").unwrap();
-    // Note: Do not check returned value since it is depending on whether current commit is already
-    // pushed or not
+    let h = g.tag_hash("0.10.1").unwrap();
+    let b = g.remote_contains(&h).unwrap();
+    assert!(b);
 }
