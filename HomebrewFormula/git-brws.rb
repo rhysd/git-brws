bf29e21a1df5b739fc57c0395106c17d999c6e51
rhysd <lin90162@yahoo.co.jp>
2020-02-08 23:53:09 +0900

    update Homebrew formula to v0.11.7

--- HomebrewFormula/git-brws.rb
+++ HomebrewFormula/git-brws.rb
@@ -1,14 +1,14 @@
 class GitBrws < Formula
-  version '0.11.6'
+  version '0.11.7'
   desc 'Command line tool to open repository, file, commit, diff, pull request or issue in browser'
   homepage 'https://github.com/rhysd/git-brws'
 
   if OS.mac?
     url "https://github.com/rhysd/git-brws/releases/download/v#{version}/git-brws-v#{version}-x86_64-apple-darwin.zip"
-    sha256 'c700385a3a470fcc137e2bc71d8c1a2b3ff5723a7da0cb081859b18036076d35' # mac
+    sha256 '0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5' # mac
   elsif OS.linux?
     url "https://github.com/rhysd/git-brws/releases/download/v#{version}/git-brws-v#{version}-x86_64-unknown-linux-gnu.zip"
-    sha256 '2c900c315a059dbd3079f5be781ef674bf0c124d1254cc986ba1c127ae4fa406' # linux
+    sha256 '0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5' # linux
   end
 
   def install
