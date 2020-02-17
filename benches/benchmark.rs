c4f60895db816bcc1bd33b007c47ee133fba1a95
rhysd <lin90162@yahoo.co.jp>
2020-02-17 12:51:24 +0900

    add more benchmark test cases

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,36 +1,27 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use git_brws::argv::Parsed;
 use git_brws::url::build_url;
-use std::ffi::OsStr;
-
-struct DummyArgs<'a>(Vec<&'a OsStr>);
-
-impl<'a> DummyArgs<'a> {
-    fn new(args: &'a [&'a str]) -> DummyArgs<'a> {
-        DummyArgs(args.iter().map(AsRef::as_ref).collect())
-    }
-}
-
-impl<'a> IntoIterator for DummyArgs<'a> {
-    type Item = &'a OsStr;
-    type IntoIter = std::vec::IntoIter<Self::Item>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.0.into_iter()
-    }
-}
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("no argument", |b| {
-        b.iter(|| {
-            let args = DummyArgs::new(&[]);
-            if let Parsed::OpenPage(cfg) = Parsed::from_iter(args).unwrap() {
-                build_url(black_box(&cfg)).unwrap();
-            } else {
-                assert!(false);
-            }
-        })
-    });
+    macro_rules! bench_function {
+        ($bench_name:expr, $args:expr) => {
+            c.bench_function($bench_name, |b| {
+                b.iter(|| {
+                    let parsed = Parsed::from_iter($args.iter()).unwrap();
+                    if let Parsed::OpenPage(cfg) = parsed {
+                        build_url(&cfg).unwrap();
+                    } else {
+                        assert!(false);
+                    }
+                })
+            });
+        };
+    }
+
+    bench_function!("no argument", ["git-brws"]);
+    bench_function!("commit", ["git-brws", "c6c470c"]);
+    bench_function!("diff", ["git-brws", "25d500f..c6c470c"]);
+    bench_function!("file", ["git-brws", "README.md"]);
 }
 
 criterion_group!(benches, criterion_benchmark);
