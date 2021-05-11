6ac6fbdf8cbb41c258925f99e5fead9dd54bbfbd
rhysd <lin90162@yahoo.co.jp>
2021-05-11 20:04:35 +0900

    upgrade tokio from v0.2 to v1.5

--- src/async_runtime.rs
+++ src/async_runtime.rs
@@ -4,13 +4,9 @@ use std::sync::Mutex;
 use tokio::runtime::{Builder, Runtime};
 
 lazy_static! {
-    static ref RUNTIME: Mutex<Runtime> = Mutex::new(
-        Builder::new()
-            .basic_scheduler() // Do not make a new thread since this runtime is only used for network requests
-            .enable_all()
-            .build()
-            .unwrap()
-    );
+    // Do not make a new thread since this runtime is only used for network requests
+    static ref RUNTIME: Mutex<Runtime> =
+        Mutex::new(Builder::new_current_thread().enable_all().build().unwrap());
 }
 
 pub fn blocking<F: Future>(future: F) -> F::Output {
