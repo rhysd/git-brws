cd76151b094d9e3061fad93a9b05931fcf813aaf
rhysd <lin90162@yahoo.co.jp>
2022-02-13 21:59:26 +0900

    update `open` crate from v1.7 to v2.0

--- src/url.rs
+++ src/url.rs
@@ -2,7 +2,7 @@ use crate::config::{Config, EnvConfig};
 use crate::error::{Error, ErrorKind, Result};
 use crate::page::parse_page;
 use crate::service;
-use std::process::{Command, ExitStatus, Stdio};
+use std::process::{Command, Stdio};
 
 pub fn build_url(cfg: &Config) -> Result<String> {
     let page = parse_page(cfg)?;
@@ -27,40 +27,15 @@ fn browse_with_cmd(url: &str, cmd: &str) -> Result<()> {
     }
 }
 
-#[cfg(unix)]
-fn error_without_status(status: ExitStatus) -> String {
-    use std::os::unix::process::ExitStatusExt;
-    if let Some(sig) = status.signal() {
-        format!("Command terminated by signal {}", sig)
-    } else {
-        "Command terminated by signal".to_string()
-    }
-}
-
-#[cfg(not(unix))]
-fn error_without_status(_status: ExitStatus) -> String {
-    "Command terminated by signal".to_string()
-}
-
 pub fn browse(url: &str, env: &EnvConfig) -> Result<()> {
     if let Some(cmd) = &env.browse_command {
         return browse_with_cmd(url, cmd);
     }
 
-    match open::that(url) {
-        Ok(status) if status.success() => Ok(()),
-        Ok(status) => {
-            let url = url.to_string();
-            let msg = if let Some(code) = status.code() {
-                format!("Command exited with non-zero status {}", code)
-            } else {
-                error_without_status(status)
-            };
-            Error::err(ErrorKind::OpenUrlFailure { url, msg })
-        }
-        Err(e) => Error::err(ErrorKind::OpenUrlFailure {
+    open::that(url).map_err(|e| {
+        Error::new(ErrorKind::OpenUrlFailure {
             url: url.to_string(),
             msg: format!("{}", e),
-        }),
-    }
+        })
+    })
 }
